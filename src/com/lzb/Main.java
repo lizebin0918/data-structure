aca5bbba988fd5d1d96840ab7badc56efe7c9a45
lizebin <lizebin0918@gmail.com>
2021-02-06 22:29:17 +0800

    初始化提交

---
+++ src/com/lzb/Main.java
@@ -0,0 +1,12 @@
+package com.lzb;
+/**
+ * 主方法<br/>
+ * Created on : 2021-02-06 22:22
+ * @author lizebin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
