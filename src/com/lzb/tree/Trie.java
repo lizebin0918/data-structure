b742ac5eaa36e733fa1b32e6005d591a15919d57
lizebin <lizebin0918@gmail.com>
2021-02-22 23:06:33 +0800

    字典树新增

--- src/com/lzb/tree/Trie.java
+++ src/com/lzb/tree/Trie.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 public class Trie<V> {
 
     private int size;
+    private Node<V> root = new Node<>();
 
     public int size() {
         return size;
@@ -32,7 +33,32 @@ public class Trie<V> {
     }
 
     public V add(String key, V value) {
-        return null;
+        char[] array = key.toCharArray();
+        Node<V> node = root;
+        V oldValue = null;
+        for (int i = 0, length = array.length; i < length; i++) {
+            Character c = array[i];
+            HashMap<Character, Node<V>> chidren = node.chidren;
+            //孩子节点不存在
+            if (chidren == null) {
+                System.out.println("c = " + c);
+                chidren = new HashMap<>();
+                Node<V> newChildNode = new Node<>();
+                chidren.put(c, newChildNode);
+                node.chidren = chidren;
+                if (i == length - 1) {
+                    newChildNode.word = true;
+                    newChildNode.value = value;
+                    break;
+                }
+            }
+            node = chidren.get(c);
+            if (node != null && i == length - 1) {
+                oldValue = node.value;
+                node.value = value;
+            }
+        }
+        return oldValue;
     }
 
     public V remove(String key) {
@@ -44,6 +70,9 @@ public class Trie<V> {
     }
 
     private static class Node<V> {
+        /**
+         * 存储对应字符，相当于树的连接线
+         */
         HashMap<Character, Node<V>> chidren;
         /**
          * 存储对应的值
@@ -52,8 +81,12 @@ public class Trie<V> {
         /**
          * 是否为单词的结尾
          */
-        boolean word;
+        boolean word = false;
     }
 
+    public static void main(String[] args) {
+        Trie<String> trie = new Trie<>();
+        System.out.println(trie.add("lizebin", "me"));
+    }
 
 }
