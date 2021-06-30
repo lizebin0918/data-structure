cfdd0f7a635bb915ce27d275288f18e56b9dfd71
lizebin <lizebin0918@gmail.com>
2021-06-30 22:36:49 +0800

    基于对象的某个字段排序

--- src/main/java/com/lzb/sort/RadixSortForPerson.java
+++ src/main/java/com/lzb/sort/RadixSortForPerson.java
@@ -1,9 +1,8 @@
 package com.lzb.sort;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import com.sun.corba.se.impl.oa.toa.TOA;
+
+import java.util.*;
 
 /**
  * 对Person.age基数排序<br/>
@@ -21,12 +20,31 @@ public class RadixSortForPerson {
             this.name = name;
             this.age = age;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 
     public static void main(String[] args) {
+        int total = 1000;
+        Random r = new Random();
         List<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            persons.add(new Person(Objects.toString(i), i));
+        for (int i = 0; i < total; i++) {
+            int age = r.nextInt(150);
+            persons.add(new Person(Objects.toString(age), age));
         }
 
         Collections.shuffle(persons);
@@ -42,6 +60,35 @@ public class RadixSortForPerson {
      */
     public static void sort(List<Person> persons) {
 
-    }
+        //persons.sort(Comparator.comparing(Person::getAge));
 
+        int bucketSize = 150;
+        int[] buckets = new int[bucketSize];
+
+        for (Person p : persons) {
+            int age = p.getAge();
+            buckets[age] = buckets[age] + 1;
+        }
+
+        //计算出每个bucket最后一个索引
+        int lastIndex = 0;
+        for (int i = 0; i < bucketSize; i++) {
+            int bucket = buckets[i];
+            if (bucket == 0) {
+                continue;
+            }
+            lastIndex += bucket;
+            buckets[i] = lastIndex;
+        }
+
+        List<Person> tempList = new ArrayList<>(persons);
+        for (Person p : tempList) {
+            int age = p.getAge();
+            int bucket = buckets[age];
+            int index = bucket - 1;
+            buckets[age] = index;
+            persons.set(index, p);
+        }
+        tempList.clear();
+    }
 }
